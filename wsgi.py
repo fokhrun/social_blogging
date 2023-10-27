d4f669424befe2511468eb80af60be8ef1447a59
Fakhrunnahar Rupa <rupa0609@gmail.com>
2023-10-27 21:44:43 +0200

    add error handler

--- wsgi.py
+++ wsgi.py
@@ -10,3 +10,7 @@ bootstrap = Bootstrap(app)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.errorhandler(404)
+def page_not_found():
+    return render_template("404.html"), 404
