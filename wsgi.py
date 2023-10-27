6940c4848d00557d5cdf5158f335f347d154f16c
Fakhrunnahar Rupa <rupa0609@gmail.com>
2023-10-27 22:19:49 +0200

    add a basic form handler

--- wsgi.py
+++ wsgi.py
@@ -1,15 +1,30 @@
 
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "passcode"
 bootstrap = Bootstrap(app)
 
 
-@app.route("/")
+class NameForm(FlaskForm):
+    name = StringField("Name", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ""
+    return render_template("index.html", form=form, name=name)
+
 
 @app.errorhandler(404)
 def page_not_found():
